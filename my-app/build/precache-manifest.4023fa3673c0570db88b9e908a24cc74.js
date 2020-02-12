63e66b1fe930468d9d06088e8f3961a4907fab33
Tania <taniameoow@gmail.com>
2020-02-12 12:45:44 +0200

    add

---
+++ my-app/build/precache-manifest.4023fa3673c0570db88b9e908a24cc74.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b72c0b35ddea11907826c9af5f15ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cdc1654e46251c01a02",
+    "url": "/static/css/3.4087c797.chunk.css"
+  },
+  {
+    "revision": "3297cc2359b72c67e70a",
+    "url": "/static/js/2.6177011a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.6177011a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cdc1654e46251c01a02",
+    "url": "/static/js/3.2275a3ed.chunk.js"
+  },
+  {
+    "revision": "3c1a91698a3d58f7eab2",
+    "url": "/static/js/4.4c3a60a7.chunk.js"
+  },
+  {
+    "revision": "c752955a9957c71a589c",
+    "url": "/static/js/5.92437b0a.chunk.js"
+  },
+  {
+    "revision": "b813ab347f9ae1913bcc",
+    "url": "/static/js/main.7ea871a0.chunk.js"
+  },
+  {
+    "revision": "4544ba7d4eba697b2c17",
+    "url": "/static/js/runtime-main.c328b4b0.js"
+  }
+]);
